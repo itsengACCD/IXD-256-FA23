d937d3ec15d0e5b9a1ee561b01345440769de782
itsengACCD <125322060+itsengACCD@users.noreply.github.com>
2023-10-27 01:48:28 -0700

    updates

--- Assignment 2/website/main.py
+++ Assignment 2/website/main.py
@@ -1,47 +1,45 @@
 import js as p5
 from js import document
 
-data = None
+data_string = None
+data_list = None
+sensor_val = None
+button_val = None
+
+# load image data and assign it to variable:
+valley_img = p5.loadImage('valley.png')
 
 def setup():
-  p5.createCanvas(500, 500)
-  print('update')
+  p5.createCanvas(800, 400)
 
 def draw():
-  p5.background(255)
+  global data_string, data_list
+  global sensor_val, button_val
 
-  global data
+  # assign content of "data" div on index.html page to variable:
   data_string = document.getElementById("data").innerText
+  # split data_string by comma, making a list:
   data_list = data_string.split(',')
-  data = data_list[0]
+
+  # assign 1st item of data_list to sensor_val:
+  sensor_val = int(data_list[0])
+  # assign 2nd item of data_list to sensor_val:
   button_val = int(data_list[1])
-  p5.text(int(data), 10, 20)
-  p5.text(button_val, 10, 30)
 
+  p5.fill(0)
   p5.noStroke()
-  p5.push()
-  # move to middle of canvas:
-  p5.translate(p5.width/2, p5.height/2)
- 
-  if(button_val == 0):
-    # set angle variable to integer of data:
-    angle = int(data)
-    # rotate canvas with angle converted from degrees to radians:
-    p5.rotate(p5.radians(angle))
-     # fill with gray responding to data:
-    p5.fill(int(data))
-    # change mode to draw rectangle from center:
-    p5.rectMode(p5.CENTER)
-    # draw rectangle at coordinate 0, 0 and 100 width and height and 20 radius:
-    p5.rect(0, 0, 100, 100, 20)
-  else:
-    circle_size = int(data)
-    p5.ellipse(0, 0, circle_size, circle_size)
-    # fill with blue color:
-    p5.fill(0, 0, 255)
-  # restore graphical transformation:
-  p5.pop()
-  
+  p5.text('sensor_val = ' + str(sensor_val), 10, 20)
+  p5.text('button_val = ' + str(button_val), 10, 35)
 
-def print_test(x):
-  print(x)
+  # change fill color with button value:
+  if(button_val == 0):
+    p5.fill(255, 200, 0)  # yellow fill
+    p5.background(sensor_val-55, sensor_val-55, sensor_val+100)
+
+  else:
+    p5.fill(255, 255, 255)  # white fill
+    p5.background(100-sensor_val, 100-sensor_val, 200-sensor_val)
+
+  # change sun height with sensor value:
+  p5.ellipse(400, 355-sensor_val, 100, 100)
+  p5.image(valley_img, 0, 0, 800, 400)
