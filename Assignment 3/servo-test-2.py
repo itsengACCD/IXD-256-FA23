57dc0d05e3af150468250e35df1675c9d8719730
itsengACCD <125322060+itsengACCD@users.noreply.github.com>
2023-10-27 11:48:38 -0700

    updates

--- Assignment 3/servo-test-2.py
+++ Assignment 3/servo-test-2.py
@@ -1,13 +1,18 @@
+# controlling a servo using servo library
 # servo.py library file should be burned on AtomS3 board
 
 from servo import Servo
 import time
 
+# configure servo on pin G7:
 servo = Servo(pin=7)
 
 while(True):
-    servo.move(65) # move slowly clockwise
-    #servo.move(100) # move slowly counterclockwise
+    servo.move(85) # move slowly clockwise
     time.sleep_ms(500)
     servo.move(90) # stop between 90-95
-    time.sleep(2)
+    time.sleep(1)
+    servo.move(103) # move slowly counterclockwise
+    time.sleep_ms(500)
+    servo.move(90) # stop between 90-95
+    time.sleep(1)
